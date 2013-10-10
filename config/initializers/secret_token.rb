9fbf7dad9558680c488336db2052e72740aace23
me-programmers <me.programmers@gmail.com>
2013-10-10 19:17:23 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppBiropum::Application.config.secret_token = 'f28f1ec8dc5099d867d26a179212a5e7f3502e2def0a601b3a1b278bb5b5e6f3115e876ac564a54847e0c8586b0258ac07b36e8496fe59756eabb17bf5619794'
